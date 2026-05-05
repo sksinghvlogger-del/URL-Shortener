f96fcca65b6d718693b613194c2e067249641231
sksinghvlogger-del <sksinghvlogger@gmail.com>
2026-05-05 23:42:59 +0530

    URLShortener.java

--- URLShortener.java
+++ URLShortener.java
@@ -1,15 +1,11 @@
 import java.util.*;
-
 public class URLShortener {
-
     private static Map<String, String> shortToLong = new HashMap<>();
     private static final String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final int SHORT_LENGTH = 6;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int choice;
-
         do {
             System.out.println("\n=== URL Shortener ===");
             System.out.println("1. Shorten URL");
@@ -17,10 +13,8 @@ public class URLShortener {
             System.out.println("3. View All URLs");
             System.out.println("0. Exit");
             System.out.print("Enter choice: ");
-
             choice = sc.nextInt();
             sc.nextLine();
-
             switch (choice) {
                 case 1:
                     System.out.print("Enter long URL: ");
@@ -28,19 +22,16 @@ public class URLShortener {
                     String shortUrl = shortenURL(longUrl);
                     System.out.println("Short URL: http://localhost:8080/" + shortUrl);
                     break;
-
                 case 2:
                     System.out.print("Enter short code: ");
                     String code = sc.nextLine();
                     String original = shortToLong.get(code);
-
                     if (original != null) {
                         System.out.println("Original URL: " + original);
                     } else {
                         System.out.println("URL not found!");
                     }
                     break;
-
                 case 3:
                     if (shortToLong.isEmpty()) {
                         System.out.println("No URLs stored.");
@@ -50,37 +41,29 @@ public class URLShortener {
                         }
                     }
                     break;
-
                 case 0:
                     System.out.println("Exiting...");
                     break;
-
                 default:
                     System.out.println("Invalid choice!");
             }
 
         } while (choice != 0);
     }
-
     private static String shortenURL(String longUrl) {
         String shortCode;
-
         do {
             shortCode = generateCode();
         } while (shortToLong.containsKey(shortCode));
-
         shortToLong.put(shortCode, longUrl);
         return shortCode;
     }
-
     private static String generateCode() {
         Random rand = new Random();
         StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < SHORT_LENGTH; i++) {
             sb.append(chars.charAt(rand.nextInt(chars.length())));
         }
-
         return sb.toString();
     }
 }
